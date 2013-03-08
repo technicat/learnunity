56e5f07d8cb50d5531e829e59a3a7324a0715f0e
technicat <github@technicat.com>
2013-03-08 10:35:04 -0800

    moved chapters

--- chapter7/unityproject/Assets/Scripts/FuguBowl.js
+++ chapter7/unityproject/Assets/Scripts/FuguBowl.js
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2013 Technicat, LLC. All Rights Reserved. MIT License
+http://github.com/technicat/LearnUnity
+*/
+
+// bowling game controller
+
 #pragma strict
 #pragma downcast
 
