b9cb08c57605b24c27dc2d458bef8ed6532edcc9
technicat <github@technicat.com>
2013-03-17 11:23:53 -0700

    iAd scripts

---
+++ chapter15/unityproject/Assets/Scripts/FuguAdBanner.js
@@ -0,0 +1,35 @@
+/*
+Copyright (c) 2013 Technicat, LLC. All Rights Reserved. MIT License.
+http://github.com/technicat/LearnUnity
+*/
+
+#pragma strict
+
+public var showOnTop:boolean = true; // banner on top or bottom of screen
+public var dontDestroy:boolean = false; // keep this GameObject around for next scene
+
+#if UNITY_IPHONE
+
+// don't make this a local variable, or else it'll get garbage collected
+private var banner:ADBannerView;
+
+function Start () {
+	if (dontDestroy) {
+		GameObject.DontDestroyOnLoad(gameObject); // keep ad alive if we load a new scene
+	}
+	Debug.Log("Creating iAd banner");
+	banner = new ADBannerView();
+	banner.autoSize = true;
+	banner.autoPosition = showOnTop ? ADPosition.Top : ADPosition.Bottom;
+	while (!banner.loaded && banner.error == null) {
+		yield;
+	}
+	if (banner.error == null) {
+		Debug.Log("iAd banner shown");
+		banner.Show();
+	} else {
+		Debug.Log("iAd banner error: "+banner.error.description);
+		banner = null;
+	}
+}
+#endif
