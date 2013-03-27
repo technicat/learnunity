3c85ea4f4d5b13f2cab07c883ef9f45311918cdc
technicat <github@technicat.com>
2013-03-26 20:03:31 -0700

    Added C# Ad and Spinner scripts

---
+++ chapter17/unityproject/Assets/Scripts/AdBanner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fugu {
+
+public class AdBanner : MonoBehaviour {
+		
+	public bool showOnTop = true;
+	public bool dontDestroy = true;
+		
+	private ADBannerView banner = null;
+		
+	void Start() {
+			StartCoroutine (StartBanner());
+	}
+		
+	// Use this for initialization
+	private IEnumerator StartBanner () {
+		if (dontDestroy) {
+				GameObject.DontDestroyOnLoad(gameObject); // keep ad alive if we load a new scene
+		}
+		banner = new ADBannerView();
+		banner.autoSize = true;
+		banner.autoPosition = showOnTop ? ADPosition.Top : ADPosition.Bottom;
+		while (!banner.loaded && banner.error == null) {
+			yield return null;
+		}
+		if (banner.error == null) {
+			banner.Show();
+		} else {
+			banner = null;
+		}
+	}
+}
+	
+} // end namespace
---
+++ chapter17/unityproject/Assets/Scripts/Spinner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fugu {
+public class Spinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad(this.gameObject);
+		StartCoroutine(StartActivityIndicator());
+	}
+		
+	IEnumerator StartActivityIndicator () {
+		#if UNITY_IPHONE
+		Handheld.SetActivityIndicatorStyle(iOSActivityIndicatorStyle.Gray);
+		#endif
+		#if UNITY_ANDROID
+		Handheld.SetActivityIndicatorStyle(AndroidActivityIndicatorStyle.InversedLarge);
+		#endif
+		Handheld.StartActivityIndicator();
+		yield return null;
+}
+
+	void OnLevelWasLoaded() {
+		Handheld.StopActivityIndicator();
+		GameObject.Destroy(gameObject);
+}
+	
+	
+}
+	
+}
