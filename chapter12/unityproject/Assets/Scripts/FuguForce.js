d2b9a283d9cfdee2650bbb08eec47e9a050df040
technicat <github@technicat.com>
2013-05-09 23:54:13 -0700

    updated FuguForce in chapter 12

--- chapter12/unityproject/Assets/Scripts/FuguForce.js
+++ chapter12/unityproject/Assets/Scripts/FuguForce.js
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2013 Technicat, LLC. All Rights Reserved. MIT License.
-http://github.com/technicat/LearnUnity
+http://learnunity4.com/
 */
 
 // HyperBowl-style bowling ball control
@@ -10,12 +10,14 @@ http://github.com/technicat/LearnUnity
 var mousepowerx:float = 1.0;
 var mousepowery:float = 1.0;
 
-var maxVelocity:float=400.0;
+var maxVelocitySquared:float=400.0;
 
 private var forcey:float=0;
 private var forcex:float=0;
 
 private var isRolling:boolean=false;
+
+private var floorTag:String = "Floor";
 
 function Update() {
 	forcex = 0;
@@ -26,31 +28,31 @@ function Update() {
 }
 
 function CalcForce() {
-	var deltatime:float = Time.deltaTime;
-	forcey = mousepowery*Input.GetAxis("Mouse Y")/deltatime;
-	forcex = mousepowerx*Input.GetAxis("Mouse X")/deltatime;
+	var deltaTime:float = Time.deltaTime;
+	forcex = mousepowerx*Input.GetAxis("Mouse X")/deltaTime;
+	forcey = mousepowery*Input.GetAxis("Mouse Y")/deltaTime;
 }
 
 function FixedUpdate() {
-	if (isRolling && rigidbody.velocity.sqrMagnitude<maxVelocity) {
+	if (isRolling && rigidbody.velocity.sqrMagnitude<maxVelocitySquared) {
 		rigidbody.AddForce(forcex,0,forcey);
 	}
 }
 
 function OnCollisionEnter(collider:Collision) {
-	if (collider.gameObject.tag == "Floor") {
+	if (collider.gameObject.tag == floorTag) {
 		isRolling = true;
 	}
 }
 
 function OnCollisionStay(collider:Collision) {
-	if (collider.gameObject.tag == "Floor") {
+	if (collider.gameObject.tag == floorTag) {
 		isRolling = true;
 	}	
 }
 
 function OnCollisionExit(collider:Collision) {
-	if (collider.gameObject.tag == "Floor") {
+	if (collider.gameObject.tag == floorTag) {
 		isRolling = false;
 	}
 }
