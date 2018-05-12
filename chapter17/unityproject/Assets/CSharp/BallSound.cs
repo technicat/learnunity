fed06a0361c65c59b2fee4cc0f849286e3898a22
Philip Chu <donotspam@technicat.com>
2018-05-12 10:48:09 -0700

    C# version of BallSound

---
+++ chapter17/unityproject/Assets/CSharp/BallSound.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fugu {}
+public class BallSound : MonoBehaviour {
+
+public float minSpeed = 1.0f;
+
+private float sqrMinSpeed = 0.0f; // the square of the minSpeed
+
+private Rigidbody body = null; // cached Rigidbody Component
+private AudioSource audiosrc; // cached AudioSource Component
+
+private string floorTag = "Floor";
+
+void Awake() {
+	body = GetComponent<Rigidbody>();
+	audiosrc = GetComponent<AudioSource>();
+	sqrMinSpeed = minSpeed*minSpeed;
+}
+
+void OnCollisionStay(Collision collider) {
+	if (collider.gameObject.tag == floorTag) {
+		if (body.velocity.sqrMagnitude>minSpeed) {
+			if (!audiosrc.isPlaying) {
+				audiosrc.Play();
+			}
+		} else {
+			if (audiosrc.isPlaying) {
+				audiosrc.Stop();
+			}
+		}	
+	}
+}
+
+void OnCollisionExit(Collision collider) {
+	if (collider.gameObject.tag == floorTag) {
+		if (audiosrc.isPlaying) {
+			audiosrc.Stop();
+		}
+	}
+}
+
+}
