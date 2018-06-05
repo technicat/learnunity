e36958f58a64cf6e432bb4ed506189d0af225e1d
Philip Chu <donotspam@technicat.com>
2018-06-05 09:52:41 -0700

    updated comments

--- chapter6/unityproject/Assets/Scripts/FuguForce.js
+++ chapter6/unityproject/Assets/Scripts/FuguForce.js
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2013 Technicat, LLC. All Rights Reserved. MIT License.
-http://learnunity4.com/
+http://technicat.com/
 */
 
 // HyperBowl-style bowling ball control
--- chapter6/unityproject/Assets/Scripts/FuguRotate.js
+++ chapter6/unityproject/Assets/Scripts/FuguRotate.js
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2013 Technicat, LLC. All Rights Reserved. MIT License.
-http://learnunity4.com/
+http://technicat.com/
 */
 
 // various ways to rotate
